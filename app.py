e03c9cc8ac3a5461f5e993871bd624e4e612d6c1
Turgay <turgaybozoglu>
2025-04-29 13:31:27 +0300

    Git

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def generate_camera_frame():
         if not success:
             break
         else:
+            frame= cv2.flip(frame,1)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame_bytes = buffer.tobytes()
             yield (b'--frame\r\n'
